aca6cfa1133bb1dd7ca6781f168de9276b1685f2
eliozashvili <ilivhsazoile.g@gmail.com>
2026-06-15 20:41:41 +0400

    feat: score ui, enemy waves, placeholder dialogue

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,12 +3,31 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private ParticleSystem destructionVfx;
+    [SerializeField] private int hitPoints;
+    [SerializeField] private int scoreValue;
+
+    private Scoreboard _scoreboard;
+
+    private void Start()
+    {
+        _scoreboard = FindAnyObjectByType<Scoreboard>();
+    }
 
     // Every single time this Enemy script is attached to a GameObject
     // with a non-trigger collider, and collision is with particle
     // it will activate this event
     private void OnParticleCollision(GameObject other)
     {
+        ProcessHitPoints();
+    }
+
+    private void ProcessHitPoints()
+    {
+        hitPoints--;
+
+        if (hitPoints > 0) return;
+
+        _scoreboard.CountScore(scoreValue);
         Instantiate(destructionVfx, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
